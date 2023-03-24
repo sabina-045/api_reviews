dd1280b0bc11040c47488dbfc50906f09c6026e9
Сабина Гаджиева <sabina_045@mail.ru>
2023-03-24 13:41:38 +0700

    Маленькие изменения

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -5,9 +5,9 @@ from api.views import TitleViewSet, CategoryViewSet, GenreViewSet, ReviewViewSet
 
 
 router_v1 = DefaultRouter()
-router_v1.register('titles', TitleViewSet, basename='titles')
-router_v1.register('categories', CategoryViewSet, basename='categories')
-router_v1.register('genres', GenreViewSet, basename='genres')
+router_v1.register(r'titles', TitleViewSet, basename='titles')
+router_v1.register(r'categories', CategoryViewSet, basename='categories')
+router_v1.register(r'genres', GenreViewSet, basename='genres')
 router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
                    ReviewViewSet, basename='reviews')
 router_v1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
